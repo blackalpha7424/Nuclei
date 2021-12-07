4f385774ded60b9f1d176b2b4fc8b7eff5fb8e9a
LuitelSamikshya <luitelsamikshya1@gmail.com>
2021-12-06 20:08:49 -0600

    path updation

--- v2/cmd/functional-test/run.sh
+++ v2/cmd/functional-test/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo 'Building functional-test binary'
-go build
+go build -o functional-test
 
 echo 'Building Nuclei binary from current branch'
 go build -o nuclei_dev ../nuclei
 
 echo 'Installing latest release of nuclei'
-GO111MODULE=on go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+go build -v -o nuclei github.com/projectdiscovery/nuclei/v2/cmd/nuclei
 
 echo 'Starting Nuclei functional test'
-./functional-test -main nuclei -dev ./nuclei_dev -testcases testcases.txt
+./functional-test -main ./nuclei -dev ./nuclei_dev -testcases testcases.txt
