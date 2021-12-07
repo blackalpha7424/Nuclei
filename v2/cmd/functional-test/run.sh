5a2e0184c8598e1222ad2906b01cb0aafdd543c6
LuitelSamikshya <luitelsamikshya1@gmail.com>
2021-12-06 20:17:40 -0600

    run.sh

--- v2/cmd/functional-test/run.sh
+++ v2/cmd/functional-test/run.sh
@@ -9,5 +9,7 @@ go build -o nuclei_dev ../nuclei
 echo 'Installing latest release of nuclei'
 go build -v -o nuclei github.com/projectdiscovery/nuclei/v2/cmd/nuclei
 
+echo $(pwd)
+echo $(ls)
 echo 'Starting Nuclei functional test'
 ./functional-test -main ./nuclei -dev ./nuclei_dev -testcases testcases.txt
