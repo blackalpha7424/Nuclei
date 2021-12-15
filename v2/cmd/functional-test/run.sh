491aa9f59719c60a950c1ebe14a47c0c5e95cd8f
LuitelSamikshya <luitelsamikshya1@gmail.com>
2021-12-14 20:21:35 -0600

    shell script changes

--- v2/cmd/functional-test/run.sh
+++ v2/cmd/functional-test/run.sh
@@ -3,77 +3,18 @@
 # reading os type from flags
 CURRENT_OS=$1
 
+if [ "${CURRENT_OS}" == "windows-latest" ];then
+    extension=.exe
+fi
+go build -o functional-test$extension
+echo 'Building functional-test binary'
+go build -o functional-test$extension
 
+echo 'Building Nuclei binary from current branch'
+go build -o nuclei_dev$extension ../nuclei
 
-# if [ "${CURRENT_OS}" == "windows-latest" ]
-# then
-#      echo 'Running on windows platform'
-#      echo 'Building functional-test binary'
-#      go build
-    
-#      echo 'Building Nuclei binary from current branch'
-#      go build -o nuclei_dev.exe ../nuclei
-    
-#      echo 'Installing latest release of nuclei'
-#      go build -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
-    
-#      echo $(pwd)
-#      echo $(ls)
-#      echo 'Starting Nuclei functional test'
-#      ./functional-test.exe -main ./nuclei.exe -dev ./nuclei_dev.exe -testcases testcases.txt
-# else
-#      echo 'Building functional-test binary'
-#      go build -o functional-test
-     
-#      echo 'Building Nuclei binary from current branch'
-#      go build -o nuclei_dev ../nuclei
-     
-#      echo 'Installing latest release of nuclei'
-#      go build -v -o nuclei github.com/projectdiscovery/nuclei/v2/cmd/nuclei
-     
-#      echo $(pwd)
-#      echo $(ls)
-#      echo 'Starting Nuclei functional test'
-#      ./functional-test -main ./nuclei -dev ./nuclei_dev -testcases testcases.txt
-# fi
+echo 'Installing latest release of nuclei'
+go build -o nuclei$extension -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
 
-     WINDOWSDEVBUILD = "nuclei_dev.exe"
-     LINUXDEVBUILD = "nuclei_dev"
-     WINDOWSBUILD = "nuclei.exe"
-     LINUXBUILD = "nuclei"
-     WINDOWSFUNCTIONALBUILD = "functional-test.exe"
-     LINUXFUNCTIONALBUILD = "functional-test"
-
-     echo 'Building functional-test binary'
-     if [ "${CURRENT_OS}" == "windows-latest" ]
-     then
-     go build -o WINDOWSFUNCTIONALBUILD
-     else 
-     go build -o LINUXFUNCTIONALBUILD
-     fi
-
-     echo 'Building Nuclei binary from current branch'
-     if [ "${CURRENT_OS}" == "windows-latest" ]
-     then
-     go build -o WINDOWSDEVBUILD ../nuclei
-     else 
-     go build -o LINUXDEVBUILD ../nuclei
-     fi
-    
-     echo 'Installing latest release of nuclei'
-     if [ "${CURRENT_OS}" == "windows-latest" ]
-     then
-     go build -v -o WINDOWSBUILD github.com/projectdiscovery/nuclei/v2/cmd/nuclei
-     else 
-     go build -v -o LINUXBUILD github.com/projectdiscovery/nuclei/v2/cmd/nuclei
-     fi
-    
-     echo $(pwd)
-     echo $(ls)
-     echo 'Starting Nuclei functional test'
-     if [ "${CURRENT_OS}" == "windows-latest" ]
-     then
-     ./WINDOWSFUNCTIONALBUILD -main ./WINDOWSBUILD -dev ./WINDOWSDEVBUILD -testcases testcases.txt
-     else 
-     ./LINUXFUNCTIONALBUILD -main ./LINUXBUILD -dev ./LINUXDEVBUILD -testcases testcases.txt
-     fi
+echo 'Starting Nuclei functional test'
+./functional-test$extension -main ./nuclei$extension -dev ./nuclei_dev$extension -testcases testcases.txt
